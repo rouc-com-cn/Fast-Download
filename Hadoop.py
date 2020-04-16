a1b12444fb4be0aba901b70e6fac93dd91f418d0
JiayangCao <57186766+JiayangCao@users.noreply.github.com>
2020-04-16 20:15:36 +0800

    Create Hadoop.py

---
+++ Hadoop.py
@@ -0,0 +1,15 @@
+#https://dhldhl-my.sharepoint.com/:u:/g/personal/vwqh1_uotago_ac_nz1/EXxl9HlVKyVOrSoH-W3fINYBbdwU0Bsun7DMFeGZi9bUTw?e=2TE75i
+
+#coding=utf-8
+
+"""
+
+  made by:rouc.com.cn
+  
+  os:centos8
+  
+  hadoop3.2
+  
+"""
+
+print("Download web:https://dhldhl-my.sharepoint.com/:u:/g/personal/vwqh1_uotago_ac_nz1/EXxl9HlVKyVOrSoH-W3fINYBbdwU0Bsun7DMFeGZi9bUTw?e=2TE75i")
